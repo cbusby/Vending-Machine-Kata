58d2639eec1b48708ec380413dec33f6700815ac
CBusby <callie_92@att.net>
2016-03-13 20:28:13 -0400

    Removed unused test methods.

--- test/unit/com/kata/vendingmachine/CoinSpec.groovy
+++ test/unit/com/kata/vendingmachine/CoinSpec.groovy
@@ -5,13 +5,6 @@ import spock.lang.Specification
 
 @TestFor(Coin)
 class CoinSpec extends Specification {
-
-	def setup() {
-	}
-
-	def cleanup() {
-	}
-
 	void "coin is valid when all inputs are given"() {
 		given:
 		Coin coin = new Coin(weight: 1.0, circumference: 1.0, dollarAmount: 1.0)
