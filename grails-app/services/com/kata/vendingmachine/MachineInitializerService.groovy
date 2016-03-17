f9e07d8a08b3501d917f4c287c479bf3bbad95d9
CBusby <callie_92@att.net>
2016-03-16 20:22:48 -0400

    Removed all unused variables from service.

--- grails-app/services/com/kata/vendingmachine/MachineInitializerService.groovy
+++ grails-app/services/com/kata/vendingmachine/MachineInitializerService.groovy
@@ -6,8 +6,8 @@ import grails.transaction.Transactional
 class MachineInitializerService {
 
 	def initializeVendingMachine() {
-		Coin quarter = new Coin(circumference: 24.26, weight: 5.67, centAmount: 25).save(flush: true)
-		Coin dime = new Coin(circumference: 17.91, weight: 2.268, centAmount: 10).save(flush: true)
-		Coin nickel = new Coin(circumference: 21.21, weight: 5.0, centAmount: 5).save(flush: true)
+		new Coin(circumference: 24.26, weight: 5.67, centAmount: 25).save(flush: true)
+		new Coin(circumference: 17.91, weight: 2.268, centAmount: 10).save(flush: true)
+		new Coin(circumference: 21.21, weight: 5.0, centAmount: 5).save(flush: true)
 	}
 }
