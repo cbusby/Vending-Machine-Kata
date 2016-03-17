ca3c0e7f6e5d925522cadff6e67b8c79ba601476
CBusby <callie_92@att.net>
2016-03-16 20:04:52 -0400

    Added a service to initialize the machine.

---
+++ grails-app/services/com/kata/vendingmachine/MachineInitializerService.groovy
@@ -0,0 +1,11 @@
+package com.kata.vendingmachine
+
+import grails.transaction.Transactional
+
+@Transactional
+class MachineInitializerService {
+
+	def serviceMethod() {
+
+	}
+}
---
+++ test/unit/com/kata/vendingmachine/MachineInitializerServiceSpec.groovy
@@ -0,0 +1,20 @@
+package com.kata.vendingmachine
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(MachineInitializerService)
+class MachineInitializerServiceSpec extends Specification {
+
+	def setup() {
+	}
+
+	def cleanup() {
+	}
+
+	void "test something"() {
+	}
+}
